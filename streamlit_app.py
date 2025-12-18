09bb0c262600a93036eb2134f3fb8f083cda55a7
attaf-kaoutar <117582389+attaf-ktr@users.noreply.github.com>
2025-12-18 21:50:39 +0100

    Update streamlit_app.py
    Call the SmoothieFroot API from the SniS App

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f"customize ur Smothie :) :cup_with_straw: {st.__version__}")
@@ -55,6 +56,8 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 
 
